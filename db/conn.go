f2ee472868f66ff65c0841f390f8c5ead6bbc812
duduomena1 <duduomena1@icloud.com>
2025-04-29 16:17:54 -0300

    Add database connection utility

---
+++ db/conn.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "user"
+	password = "password"
+	dbname   = "bootcamp_db"
+)
+
+func ConnectDB() (*sql.DB, error) {
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("Connected to " + dbname)
+	return db, nil
+}
