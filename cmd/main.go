2f6d8918dc38c7ff76d77dde11a99a25e27ad730
duduomena1 <duduomena1@icloud.com>
2025-04-29 16:19:14 -0300

    Add product retrieval endpoint to the API
    Integrate database connection, repository, use case, and controller layers for handling product data. Introduced a new "/products" endpoint that retrieves products using the newly added layers.

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"go-api/controller"
+	"go-api/db"
+	"go-api/repository"
+	"go-api/usecase"
 )
 
 func main() {
 
 	server := gin.Default()
 
+	dbConnection, err := db.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+	ProductRepository := repository.NewProductRepository(dbConnection)
+	ProductUseCase := usecase.NewProductUseCase(ProductRepository)
+	productController := controller.NewProductController(ProductUseCase)
+
 	server.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+	server.GET("/products", productController.GetProducts)
 
 	server.Run(":8000")
 }
