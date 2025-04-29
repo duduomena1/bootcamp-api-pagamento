4e9aa3bd9b134da8baadbe7f65a34bc74ed6aa89
duduomena1 <duduomena1@icloud.com>
2025-04-29 16:18:21 -0300

    Add ProductRepository with Get and Insert methods

---
+++ repository/product_repository.go
@@ -0,0 +1,58 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+	"go-api/models"
+)
+
+type ProductRepository struct {
+	connection *sql.DB
+}
+
+func NewProductRepository(connection *sql.DB) ProductRepository {
+	return ProductRepository{
+		connection: connection,
+	}
+}
+
+func (pr *ProductRepository) GetProducts() ([]models.Product, error) {
+	query := "SELECT id, product_name, price FROM product"
+	rows, err := pr.connection.Query(query)
+	if err != nil {
+		fmt.Println(err)
+		return []models.Product{}, err
+	}
+	var productList []models.Product
+	var productObj models.Product
+
+	for rows.Next() {
+		err = rows.Scan(&productObj.ID, &productObj.Name, &productObj.Price)
+		if err != nil {
+			fmt.Println(err)
+			return []models.Product{}, err
+		}
+		productList = append(productList, productObj)
+	}
+
+	rows.Close()
+	return productList, nil
+}
+
+func (pr *ProductRepository) InsertProduct(product models.Product) (int, error) {
+
+	var id int
+	query, err := pr.connection.Prepare("INSERT INTO product (product_name, price) VALUES ($1, $2) RETURNING id")
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+	err = query.QueryRow(product.Name, product.Price).Scan(&id)
+
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+	query.Close()
+	return id, nil
+}
